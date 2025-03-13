e69689678368ee17aafca950c80143e2527458be
marleyinbloom <marleyinbloom@gmail.com>
2025-03-13 17:33:26 -0400

    Restarting using Neoforge 1.21.1

--- fabric/src/main/java/io/itch/marleyinbloom/hold_w_to_ponder/fabric/ExampleExpectPlatformImpl.java
+++
@@ -1,9 +0,0 @@
-package io.itch.marleyinbloom.hold_w_to_ponder.fabric;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class ExampleExpectPlatformImpl {
-	public static String platformName() {
-		return FabricLoader.getInstance().isModLoaded("quilt_loader") ? "Quilt" : "Fabric";
-	}
-}
--- forge/src/main/java/io/itch/marleyinbloom/hold_w_to_ponder/forge/ExampleExpectPlatformImpl.java
+++
@@ -1,7 +0,0 @@
-package io.itch.marleyinbloom.hold_w_to_ponder.forge;
-
-public class ExampleExpectPlatformImpl {
-	public static String platformName() {
-		return "Forge";
-	}
-}
